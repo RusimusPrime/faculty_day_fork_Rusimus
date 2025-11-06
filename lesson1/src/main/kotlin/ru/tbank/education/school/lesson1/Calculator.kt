d8068f9230802447c8262cdbb931af1130f356a1
RusimusPrime <ruslankosov9@gmail.com>
2025-11-06 13:23:24 +0300

    Итоговая версия калькулятора

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -4,7 +4,12 @@ package ru.tbank.education.school.lesson1
  * Метод для вычисления простых арифметических операций.
  */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    return when (operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> if (b != 0.0) a / b else null
+    }
 }
 
 /**
@@ -14,5 +19,24 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? {
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val sss = this.split(" ")
+    val op = when (sss[1]) {
+        "+" -> OperationType.ADD
+        "-" -> OperationType.SUBTRACT
+        "*" -> OperationType.MULTIPLY
+        "/" -> OperationType.DIVIDE
+        else -> return null
+    }
+    return when (sss.size) {
+        3 -> when (sss[0].toDoubleOrNull() != null) {
+            true -> when (sss[2].toDoubleOrNull() != null) {
+                true -> calculate(sss[0].toDouble(), sss[2].toDouble(), op)
+                false -> null
+            }
+
+            false -> null
+        }
+
+        else -> null
+    }
 }
