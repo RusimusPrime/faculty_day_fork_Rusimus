9f1ac13d4c440f7545bd8f26f0f9ec0e1666fb42
RusimusPrime <ruslankosov9@gmail.com>
2025-11-06 10:48:30 +0300

    Мои первые шаги

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,6 +1,49 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    print("Hello World!")
-    println("Hello World!")
+//    print("Hello World!")
+//    println("Hello World!")
+//    println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa")
+//
+//    val a = 1
+//    val b = 's' // char
+//    val c = "s" // string
+//    val d = 1.23
+//    val e = """Организация проекта
+//Структура проекта
+//Каждый модуль проекта - отдельный урок согласно плану занятий.
+//
+//Описание занятия и его задания находятся в соответствующей директории.
+//
+//Сборка проекта
+//Проект организован как multi project build с использованием buildSrc
+//
+//Подробнее в документации Gradle
+//
+//Пример официальной документации
+//
+//Пример инициализации проекта через gradle init
+//
+//Соглашения по проекту
+//Проверка форматирования - detekt
+//Написание юнит тестов - junit5
+//Мокирование зависимостей и проверка вызовов - mockk
+//Проверка структуры проекта - archunit
+//Проверка структуры классов и наименований - konsist""".trimIndent()
+//    val f = true
+//    val g = "hello $d world"
+//    println("$a $b $c $d $e")
+//    val h: Byte = 1
+//    // val не изменятеся, var изменяется
+
+    val str = "Ruslan"
+    println("My name is $str")
+
+    var number = 5
+    println("before: $number")
+    number++
+    println("after: $number")
+
+    val unknown = null
+    println("unknown: $unknown")
 }
